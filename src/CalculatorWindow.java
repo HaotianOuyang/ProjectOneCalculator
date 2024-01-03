5d4bcfb4642ec0fcfc22300cce89a4e7179107df
HaotianOuyang <1319310146@qq.com>
2024-01-04 00:13:06 +1000

    Initialised app's window

---
+++ src/CalculatorWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class CalculatorWindow extends JFrame {
+    public CalculatorWindow(){
+         setTitle("My JFrame");
+
+         setSize(300,500);
+
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+         setVisible(true);
+
+    }
+}
